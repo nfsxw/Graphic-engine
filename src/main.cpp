af686ccc7b2bbf0673019d4ae7645b4d277fe782
Nikita Surkov <nikdarthvader@gmail.com>
2025-12-17 17:44:28 +0300

    GLAD added and tested, .gitignore expandeded

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,40 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!" <<std::endl;
+    if (!glfwInit()) {
+        std::cout << "GLFW init failed\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Window creation failed\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Вот тут Glad инициализуется
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "GLAD init failed\n";
+        return -1;
+    }
+
+    std::cout << "OpenGL version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
