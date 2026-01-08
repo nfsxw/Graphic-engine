86a4d675a370c3203e682e2de471d3c1abcfce1d
nikita <nikdarthvader@gmail.com>
2026-01-09 01:50:17 +0300

    made the window load

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main() {
     // GLFW required version:
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Window creation:
